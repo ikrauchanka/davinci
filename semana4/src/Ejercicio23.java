ff35b8d125a6f04c41dadb2dcc86b26386d3af45
ikrauchanka <11536410+ikrauchanka@users.noreply.github.com>
2024-09-14 15:08:48 -0300

    Semana 5, 4 ejercicios https://dvcampus.davinci.edu.ar/pluginfile.php/231489/mod_folder/content/0/AC1-S0D13-desaf%C3%ADo-05.pdf

---
+++ semana4/src/Ejercicio23.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+    /* Realiza un programa que permita ingresar
+       un número entero e indique si se trata de un número par o impar.
+     */
+
+        float precioFinal;
+        precioMismo = precioInicio + pricioDelTrabajo;
+        precioMedio = precioMismo + precioMismo * 45 / 100;
+
+        #3 #4 pricioDelTrabajo = 75%;
+        #1 #5 pricioDelTrabajo = 85%;
+        #2 #6 pricioDelTrabajo = 90%;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese un número entero: ");
+        numero = input.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El numero " + numero + " es par");
+        } else {
+            System.out.println("El numero: " + numero + " es impar");
+        }
+
+    }
+}
